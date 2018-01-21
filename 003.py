4cef79976d00343a8eb9a2d04e80911ccf62be67
KennethJoohyun Han <kenneth.jh.han@gmail.com>
2018-01-21 22:43:55 +0900

    Create 003.py

---
+++ 003.py
@@ -0,0 +1,12 @@
+# Bioinformatics 101 - 003.Operators
+# Python3 Source Code
+
+num1 = 7
+num2 = 2
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+print(num1 ** num2)
