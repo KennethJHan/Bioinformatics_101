3206f5178d89542a1b99470cbdbbe6418be20ef7
KennethJoohyun Han <kenneth.jh.han@gmail.com>
2018-01-21 23:39:05 +0900

    Create P004.java

---
+++ P004.java
@@ -0,0 +1,13 @@
+class P004 {
+  public static void main(String [] args) {
+    int num1 = 7;
+
+    if(num1 % 3 == 0) {
+      System.out.println("Multiple of 3");
+    } else if(num1 % 7 == 0) {
+      System.out.println("Multiple of 7");
+    } else {
+      System.out.println("None of them");
+    }
+  }
+}
