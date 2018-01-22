bd69ce47f0996bc5f7f7d8ecdc90163a820cb771
KennethJoohyun Han <kenneth.jh.han@gmail.com>
2018-01-22 17:30:24 +0900

    Create P009.java

---
+++ P009.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class P009 {
+  public static void main(String[] args) {
+    //System.out.println("Arguments: "+Arrays.toString(args)); // Print Array Elements
+    String name = args[0];
+    System.out.println("Hello "+name);
+  }
+}
