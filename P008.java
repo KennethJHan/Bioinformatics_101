66f571e2d90c5eaf257e526afaf80d82df0a6f7f
KennethJoohyun Han <kenneth.jh.han@gmail.com>
2018-01-22 17:12:22 +0900

    Create P008.java

---
+++ P008.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class P008 {
+  public static void main(String[] args) {
+    System.out.print("Enter name: ");
+    Scanner scan = new Scanner(System.in);
+    String name = scan.nextLine();
+    System.out.println("Hello "+name);
+  }
+}
