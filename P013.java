a0689edc2a36c9d9202e0072063d60cba7e7121b
KennethJoohyun Han <kenneth.jh.han@gmail.com>
2018-01-23 09:59:00 +0900

    Create P013.java

---
+++ P013.java
@@ -0,0 +1,10 @@
+class P013 {
+  public static void main(String[] args) {
+    String a = "Bio";
+    String b = "Informatics";
+    String c = a + b;
+    //String c = a.concat(b); // Another way to concatenate two strings
+
+    System.out.println(c);
+  }
+}
