7dca522a80aaa248fb21b62b599ab0d76ee8b680
KennethJoohyun Han <kenneth.jh.han@gmail.com>
2018-01-24 00:21:52 +0900

    Create P032.java

---
+++ P032.java
@@ -0,0 +1,9 @@
+class P032 {
+  public static void main(String[] args) {
+    double[] WT = {58.7, 57.4, 57.9, 57.5, 58.1, 57.3, 58.3, 57.5, 57.8, 58.1};
+    double[] Ob = {84.5, 87.2, 85.7, 88.7, 85.4, 85.6, 87.1, 86.9, 88.2, 86.5};
+
+    System.out.println(WT[1]);
+    System.out.println(Ob[1]);
+  }
+}
