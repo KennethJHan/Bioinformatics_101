22ca5077208c2b9db949a63cae6f1808dc3f7d59
KennethJoohyun Han <kenneth.jh.han@gmail.com>
2018-01-23 13:44:18 +0900

    Create P017.java

---
+++ P017.java
@@ -0,0 +1,7 @@
+class P017 {
+  public static void main(String[] args) {
+    String Seq1 = "ATGttATaG";
+    System.out.println(Seq1.toUpperCase());
+    System.out.println(Seq1.toLowerCase());
+  }
+}
