d888235763061a63d35e57b1b1717c24dcf3ab64
KennethJoohyun Han <kenneth.jh.han@gmail.com>
2018-01-23 14:58:08 +0900

    Create P018.java

---
+++ P018.java
@@ -0,0 +1,8 @@
+class P018 {
+  public static void main(String[] args) {
+    String Seq1 = "AGTTTATAG";
+    for(int i=0;i<Seq1.length();i+=3){
+      System.out.print(Seq1.charAt(i));
+    }
+  }
+}
