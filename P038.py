a25ba4e61c0cc0f74c8526428e8345ad71a5e3b6
KennethJoohyun Han <kenneth.jh.han@gmail.com>
2018-01-24 12:43:08 +0900

    Create P038.py

---
+++ P038.py
@@ -0,0 +1,6 @@
+WT = [58.7, 57.4, 57.9, 57.5, 58.1, 57.3, 58.3, 57.5, 57.8, 58.1]
+Ob = [84.5, 87.2, 85.7, 88.7, 85.4, 85.6, 87.1, 86.9, 88.2, 86.5]
+
+print("Type,Sum,Average")
+print("WT,"+str(sum(WT))+","+str(sum(WT)/len(WT)))
+print("Ob,"+str(sum(Ob))+","+str(sum(Ob)/len(Ob)))
