59a6fe608d9baa73f714d223f0816f0c5743f428
KennethJoohyun Han <kenneth.jh.han@gmail.com>
2018-01-24 11:52:35 +0900

    Create P035.py

---
+++ P035.py
@@ -0,0 +1,6 @@
+MF = ["M","F","F","M","F","M"]
+print(MF) # ['M', 'F', 'F', 'M', 'F', 'M']
+MF.append("M")
+print(MF) # ['M', 'F', 'F', 'M', 'F', 'M', 'M']
+del MF[6]
+print(MF) # ['M', 'F', 'F', 'M', 'F', 'M']
